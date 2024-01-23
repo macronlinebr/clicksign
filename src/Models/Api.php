baa1d36caecd74ddcc05e5de825d2fea4bc491ee
LuisFK <cyberlpkf@gmail.com>
2024-01-23 13:56:05 -0300

    - Code cleanup

--- src/Models/Api.php
+++ src/Models/Api.php
@@ -2,7 +2,6 @@
 
 namespace Cyberlpkf\Clicksign\Models;
 
-use App\Scopes\ApiScope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
